d01699449e0b6de3753146a0cad4bf945dc99749
Steven Black <github@springsurprise.com>
2017-06-03 15:15:05 -0700

    Major rewrite - broken into more discrete functionality - more command line switches for performing select functionality - update the perms & owner on execution - option to specify directory for spam/ham/retrain scans

--- spamscan.sh
+++ spamscan.sh
@@ -13,92 +13,155 @@
 # 2017-05-25 - 1.5.1 Remove option for log directory
 # 2017-05-27 - 1.5.2 Run sa-learn as user postfix
 # 2017-05-28 - 1.5.3 Force ownership back to MailScanner defaults for Bayes dbs
+# 2017-06-01 - 2.0.0 break it down into more discreet actions
 #
 #######################
 
 
+# defaults
+SCANSPAM=0
+SCANHAM=0
+SCANRETRAIN=0
+DUMPBAYESINFO=0
+BACKUPBAYESDB=0
+
 # process the command line options
-# the mailbox directory pattern *must* be specified
 # the pattern  *must* be wrapped in quotes when passed in to avoid premature globbing
 # the backup directory path is optional; no backup is performed if omitted
-while getopts m:b: OPTLABEL
+while getopts b:hH:im:rR:sS: OPTLABEL
 do 
   case "$OPTLABEL" in
-    m) MAILBOXDIR="$OPTARG";;
-    b) BACKUPDIR="$OPTARG";;
-    [?]) echo "Usage $0 [-m mailboxdir] [-b backupdir]" >&2
-         exit 1;;
+    b) BACKUPDIR="$OPTARG"
+       ;;
+    h) SCANHAM=1
+       ;;
+    H) SCANHAM=1; HAMDIR="$OPTARG"  
+       ;;
+    i) DUMPBAYESINFO=1
+       ;;
+    m) MAILBOXDIR="$OPTARG"
+       ;;
+    r) SCANRETRAIN=1
+       ;;
+    R) SCANRETRAIN=1; RETRAINDIR="$OPTARG"
+       ;;
+    s) SCANSPAM=1
+       ;;
+    S) SCANSPAM=1; SPAMDIR="$OPTARG" 
+       ;;
+    [?]) echo "Usage $0 [-i] [-m mailboxdir] [-b backupdir] [-h | -H hamdir] [-r | -R retraindir] [-s | -S spamdir]" >&2
+        exit 1
+        ;;
   esac
 done
 
 # If MAILBOXDIR isn't set (or is null), then use default value
-# Assume no backup desired
 MAILBOXDIR=${MAILBOXDIR:="/volume1/homes/*/.Maildir"}
-MAKEBACKUP=0;
 MAILSERVER="/var/packages/MailServer/target"
 MAILPERLLIB="${MAILSERVER}/lib/perl5" 
 DBPATH="/var/spool/MailScanner/spamassassin"
 
-# if the mailbox pattern wasn't specified, then error out
-if [ -z "$MAILBOXDIR" ]; then
-  echo "FATAL: The mailbox directory path pattern (-m) must be specified" >&2
-  exit 1
-fi
+# ensure requirements are met for making backup
+verify_backup_dir () {
+  if [ ! -z "$BACKUPDIR" ]; then
+    if [ ! -d "$BACKUPDIR" ]; then
+      echo "FATAL: Backup directory does not exist (\"$BACKUPDIR\")" >&2
+      exit 1
+    fi
 
-# figure out if a backup is desired and can be created
-if [ ! -z "$BACKUPDIR" ]; then
-  if [ ! -d "$BACKUPDIR" ]; then
-    echo "FATAL: Backup directory does not exist (\"$BACKUPDIR\")" >&2
-    exit 1
-  fi
-
-  if [ ! -w "$BACKUPDIR" ]; then
-    echo "FATAL: Unable to write to backup directory (\"$BACKUPDIR\")" >&2
-    exit 1
-  fi
+    if [ ! -w "$BACKUPDIR" ]; then
+      echo "FATAL: Unable to write to backup directory (\"$BACKUPDIR\")" >&2
+      exit 1
+    fi
   
-  # backup prerequisites have been met; try to make backup
-  MAKEBACKUP=1; 
-fi
+    # backup prerequisites have been met; try to make backup
+    BACKUPBAYESDB=1; 
+  fi
+}
 
-scan_mail () {
-  echo "Mailbox path pattern: \"${MAILBOXDIR}\""
+# sync the bayes_journal file into the bayes token db
+sync_bayes_db () {
+  echo "Syncing Bayes DB"
+  echo "Bayes DB location: \"$DBPATH\""
 
-  echo "Scanning Spam folder(s)"
-  perl -T -Mlib="${MAILPERLLIB}" "${MAILSERVER}/bin/sa-learn" \
-        --username=postfix \
-        --siteconfigpath "${MAILSERVER}/etc/spamassassin" \
-        --dbpath "${DBPATH}" \
-        --no-sync \
-        --spam "${MAILBOXDIR}/.Junk/{new,cur}"
-
-  echo "Scanning Ham folder(s)"
-  perl -T -Mlib="${MAILPERLLIB}" "${MAILSERVER}/bin/sa-learn" \
-        --username=postfix \
-        --siteconfigpath "${MAILSERVER}/etc/spamassassin" \
-        --dbpath "${DBPATH}" \
-        --no-sync \
-        --ham "${MAILBOXDIR}/{new,cur}"
-
-  # Need to do the sync now as the Spam & Ham checks were performed no-sync for faster processing
-  echo "Syncing SpamAssassin DB"
   perl -T -Mlib="${MAILPERLLIB}" "${MAILSERVER}/bin/sa-learn" \
         --username=postfix \
         --siteconfigpath "${MAILSERVER}/etc/spamassassin" \
         --dbpath "${DBPATH}" \
         --sync
+}
 
-  # Forgetting requires read/write access to the DB and so cannot be called w/ no-sync
-  echo "Scanning Retrain folder(s)"
+# scan mailboxes and learn as spam
+scan_spam () {
+  if [ -z "$SPAMDIR" ]; then
+    SCANDIR="${MAILBOXDIR}/.Junk/{new,cur}"
+  else
+    SCANDIR="$SPAMDIR"
+  fi
+
+  echo "Scanning Spam folder(s)"
+  echo "Spam path pattern: \"${SCANDIR}\""
+
   perl -T -Mlib="${MAILPERLLIB}" "${MAILSERVER}/bin/sa-learn" \
         --username=postfix \
         --siteconfigpath "${MAILSERVER}/etc/spamassassin" \
         --dbpath "${DBPATH}" \
-        --forget "${MAILBOXDIR}/.Retrain/{new,cur}"
+        --no-sync \
+        --spam "${SCANDIR}"
+
+  # The scan is performed with the no-sync option for improved speed
+  # but the results still need to be eventually synced to the Bayes DB
+  sync_bayes_db
 }
 
-dump_status () {
-  echo "Current Status"
+# scan mailboxes and learn as ham
+scan_ham () {
+  if [ -z "$HAMDIR" ]; then
+    SCANDIR="${MAILBOXDIR}/{new,cur}" 
+  else
+    SCANDIR="$HAMDIR"
+  fi
+
+  echo "Scanning Ham folder(s)"
+  echo "Ham path pattern: \"${SCANDIR}\""
+
+  perl -T -Mlib="${MAILPERLLIB}" "${MAILSERVER}/bin/sa-learn" \
+        --username=postfix \
+        --siteconfigpath "${MAILSERVER}/etc/spamassassin" \
+        --dbpath "${DBPATH}" \
+        --no-sync \
+        --ham "${SCANDIR}"
+
+  # The scan is performed with the no-sync option for improved speed
+  # but the results still need to be eventually synced to the Bayes DB
+  sync_bayes_db
+}
+
+# scan mailboxes for any retraining (misidentified spam *or* ham)
+scan_retrain () {
+  if [ -z "$RETRAINDIR" ]; then
+    SCANDIR="${MAILBOXDIR}/.Retrain/{new,cur}"
+  else
+    SCANDIR="$RETRAINDIR"
+  fi
+
+  echo "Scanning Retrain folder(s)"
+  echo "Retraining path pattern: \"${SCANDIR}\"" 
+
+  # "Forgetting" requires read/write access to the Bayes DB and 
+  # manages its own syncing. No need to explicitly sync afterwards
+  perl -T -Mlib="${MAILPERLLIB}" "${MAILSERVER}/bin/sa-learn" \
+        --username=postfix \
+        --siteconfigpath "${MAILSERVER}/etc/spamassassin" \
+        --dbpath "${DBPATH}" \
+        --forget "${SCANDIR}"
+}
+
+# print out the status of the bayes db
+dump_bayes_info () {
+  echo "Current Bayes Info"
+  echo "Bayes DB location: \"$DBPATH\""
+
   perl -T -Mlib="${MAILPERLLIB}" "${MAILSERVER}/bin/sa-learn" \
         --username=postfix \
         --siteconfigpath "${MAILSERVER}/etc/spamassassin" \
@@ -106,12 +169,11 @@ dump_status () {
         --dump magic
 }
 
-backup_spamdb () {
-  if [ $MAKEBACKUP -lt 1 ]; then
-    return
-  fi
-
+# make a backup copy of the bayes db
+backup_bayes_db () {
   echo "Backing up SpamAssassin DB"
+  echo "Backup Bayes DB location: \"${BACKUPDIR}/spamassassin.backup\""
+
   perl -T -Mlib="${MAILPERLLIB}" "${MAILSERVER}/bin/sa-learn" \
         --username=postfix \
         --siteconfigpath "${MAILSERVER}/etc/spamassassin" \
@@ -119,14 +181,22 @@ backup_spamdb () {
         --backup > "${BACKUPDIR}/spamassassin.backup"
 }
 
+# Run prerequisite checks before proceeding with scan
+if [ -n $BACKUPDIR ]; then verify_backup_dir; fi
+
 echo "==== Scan Start: $(date) ===="
-scan_mail 
-dump_status
-backup_spamdb
+
+if [ $SCANSPAM -ne 0 ]; then scan_spam; fi
+if [ $SCANHAM -ne 0 ]; then scan_ham; fi
+if [ $SCANRETRAIN -ne 0 ]; then scan_retrain; fi
+if [ $BACKUPBAYESDB -ne 0 ]; then backup_bayes_db; fi
+if [ $DUMPBAYESINFO -ne 0 ]; then dump_bayes_info; fi
 
 # Make sure set ownership back to the default so that
 # the MailScanner service can access the database
 chown postfix:root "${DBPATH}"/bayes_*
+chmod 666 "${DBPATH}"/bayes_*
+
 echo "==== Scan Complete: $(date) ===="
 
 exit 0
